c755d06dbd2b929fbdd091a507e8bf84eeda860c
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-05-24 12:34:19 -0300

    chore: add styled components, default theme and global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>hello world</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>sadasdas</h1>
+    </ThemeProvider>
+  )
 }
