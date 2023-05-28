a059e4d99faf55beccac90acad7ace7a13500b32
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-05-28 00:29:22 -0300

    style: change main to div in home container and decrease margin bottom

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main`
+export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 
   gap: 3rem;
 
-  margin-bottom: 10rem;
+  margin-bottom: 3rem;
 `
