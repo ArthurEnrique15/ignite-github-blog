e9366ccc0c436a3a16eb16c3f61de55a7c06c7d9
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-05-27 22:54:43 -0300

    chore: remove unused firebase installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 import { GithubUserProvider } from './contexts/GithubUserContext'
 import { IssuesProvider } from './contexts/IssuesContext'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { setupFirebase } from './lib/firebase'
 import { Router } from './Router'
 
 export function App() {
-  useEffect(() => {
-    setupFirebase()
-  }, [])
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
